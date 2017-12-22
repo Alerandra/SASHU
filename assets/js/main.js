a7a217436667e8821090bd633369b9ff293c4272
Александра Суворова <aleksandrasuvorova@MacBook-Pro-Aleksandra.local>
2017-12-23 00:13:50 +0300

    ыыы

--- assets/js/main.js
+++ assets/js/main.js
@@ -252,6 +252,26 @@ var settings = {
 
 	};
 
+	var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+  var template_id = "template_OjeNf1im";
+
+  myform.find("button").text("Отправка...");
+  emailjs.sendForm(service_id,template_id,"myform")
+  	.then(function(){
+    	alert("Sent!");
+       myform.find("button").text("Отправить");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Отправлено");
+    });
+  return false;
+});
+
 	$(function() {
 
 		var	$window 	= $(window),
