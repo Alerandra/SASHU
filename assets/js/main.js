8b86724fb907fbac1cc8b81dc7eb49ca999b1d17
Александра Суворова <aleksandrasuvorova@MacBook-Pro-Aleksandra.local>
2017-12-23 00:21:43 +0300

    лт

--- assets/js/main.js
+++ assets/js/main.js
@@ -271,6 +271,13 @@ myform.submit(function(event){
     });
   return false;
 });
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.emailjs.com/dist/email.min.js"></script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init("user_Re59guUW9HGmbEfAxwgZV");
+   })();
+</script>
 
 	$(function() {
 
