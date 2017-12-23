a77ffe629735f68ce7374863b747d876733813ea
Александра Суворова <aleksandrasuvorova@MacBook-Pro-Aleksandra.local>
2017-12-23 10:13:41 +0300

    п

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#slider").owlCarousel({
-          navigation : true, // Show next and prev buttons
-          slideSpeed : 300,
-          paginationSpeed : 400,
-          singleItem:true
-    });
-});
