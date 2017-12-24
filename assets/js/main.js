f3df2b6ea40ff019d4c499480a1f6d5a39dddd14
Александра Суворова <aleksandrasuvorova@MacBook-Pro-Aleksandra.local>
2017-12-24 20:34:21 +0300

    лл

--- assets/js/main.js
+++ assets/js/main.js
@@ -280,14 +280,6 @@ myform.submit(function(event){
 			$header 	= $('#header'),
 			$banner 	= $('.banner');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
 
 		// Prioritize "important" elements on medium.
 			skel.on('+medium -medium', function() {
