47c865e50c8fa2419045bb0e4cb5dfd660bedc29
Александра Суворова <aleksandrasuvorova@MacBook-Pro-Aleksandra.local>
2017-12-24 20:13:49 +0300

    сс

--- assets/js/main.js
+++ assets/js/main.js
@@ -332,15 +332,5 @@ myform.submit(function(event){
 			}
 
 	});
-	
-</script>	
-    var smoothJumpUp = function() {
-        if (document.body.scrollTop>0 || document.documentElement.scrollTop>0) {
-            window.scrollBy(0,-50);
-            setTimeout(smoothJumpUp, 20);
-        }
-    }
 
-</script>
-
-
+})(jQuery);
