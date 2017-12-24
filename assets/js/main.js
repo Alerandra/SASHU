e80023c01ad5cb8307225eeaa268ec54f558d7a8
Alerandra <33829258+Alerandra@users.noreply.github.com>
2017-12-24 20:13:16 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -332,5 +332,15 @@ myform.submit(function(event){
 			}
 
 	});
+	
+</script>	
+    var smoothJumpUp = function() {
+        if (document.body.scrollTop>0 || document.documentElement.scrollTop>0) {
+            window.scrollBy(0,-50);
+            setTimeout(smoothJumpUp, 20);
+        }
+    }
+
+</script>
 
 
